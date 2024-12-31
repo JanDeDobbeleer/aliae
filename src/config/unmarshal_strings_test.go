6c11cf1533b593efcf52b5b7a36f814c3e533c77
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2024-12-31 22:27:10 +0100

    fix(config): implement custom string unmarshaler
    resolves #210

---
+++ src/config/unmarshal_strings_test.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUnmarshalStrings(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{name: "standard string", input: "test", expected: "test"},
+		{
+			name:     "folded block scalar",
+			expected: "one two three",
+			input: `>
+      one
+	  two
+	  three`,
+		},
+		{
+			name: "literal block scalar",
+			expected: `one
+two`,
+			input: `|
+      one
+      two`,
+		},
+	}
+
+	for _, tc := range tests {
+		var result string
+		_ = stringUmarshaler(&result, []byte(tc.input))
+		assert.Equal(t, tc.expected, result)
+	}
+}
